c44f7df79fc807da3df6932dc899bd59fa8ade9f
lorenzo weber <lcweber@inf.ufsm.br>
2024-10-22 15:20:20 -0300

    changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css'; 
-import logo from "../assets/logo.png"
+import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
     <header>
       <nav>
-        <Link to="/landing" ><img src={logo} className={styles.logo}/></Link>
+        <Link to="/landing"><img src={logo} className={styles.logo} alt=''/></Link>
         <ul>
             <Link to="/login"><li>Login</li></Link>
-            <Link to="/about"><li>Sobre</li></Link>
+            <Link to="/landing#sobre"><li>Sobre</li></Link>
             <Link to="/contato"><li>Contato</li></Link>
         </ul>
       </nav>
