713c6417fc088502d23e7b7b757860b863a93bf1
Gregori Rubin <gregori.rubin@gmail.com>
2024-10-24 19:28:51 -0300

    feat: add login page

--- src/pages/Public/Login/Login.tsx
+++ src/pages/Public/Login/Login.tsx
@@ -1,10 +1,52 @@
-import React from 'react';
-import Header from '../../../components/Header'; 
+import React, { useState } from 'react';
+import Header from '../../../components/Header';
+
+import styles from './Login.module.css';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-     {<Header />}
+    <div style={{ height: '100%' }}>
+      <Header />
+      <div className={styles.body}>
+        <div className={styles.formContainer}> {/* Aplicando a classe para estilização */}
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="email">Email:</label>
+              <input
+                type="text"
+                id="email"
+                name="email"
+                placeholder='Digite seu email'
+                className={styles.input}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Senha:</label>
+              <input
+                type="password"
+                placeholder='Digite seu senha'
+                id="password"
+                className={styles.input}
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit">Fazer Login</button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 };
