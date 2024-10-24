dd9c78a549d3bb270ca49a042fc1cad6f34524e7
Gregori Rubin <gregori.rubin@gmail.com>
2024-10-24 19:30:33 -0300

    feat: center login form

--- src/pages/Public/Contato/Contato.tsx
+++ src/pages/Public/Contato/Contato.tsx
@@ -1,70 +1,76 @@
 import React, { useState } from 'react';
 import styles from './Contato.module.css'; // Importando o CSS
-import Header from '../../../components/Header'; 
+import Header from '../../../components/Header';
 
-const Contato = () => { 
-    const [formData, setFormData] = useState({
-        nome: '',
-        email: '',
-        mensagem: ''
+const Contato = () => {
+  const [formData, setFormData] = useState({
+    nome: '',
+    email: '',
+    mensagem: ''
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
     });
+  };
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value,
-        });
-    };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('Dados do formulário:', formData);
+  };
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        console.log('Dados do formulário:', formData);
-    };
-
-    return (
-        <>
-        <Header />
-        <div className={styles.body}>
-            <div className={styles.formContainer}> {/* Aplicando a classe para estilização */}
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <label htmlFor="nome">Nome:</label>
-                        <input 
-                            type="text" 
-                            id="nome" 
-                            name="nome" 
-                            value={formData.nome} 
-                            onChange={handleChange} 
-                            required 
-                            />
-                    </div>
-                    <div>
-                        <label htmlFor="email">Email:</label>
-                        <input 
-                            type="email" 
-                            id="email" 
-                            name="email" 
-                            value={formData.email} 
-                            onChange={handleChange} 
-                            required 
-                            />
-                        </div>
-                        <div>
-                        <label htmlFor="mensagem">Mensagem:</label>
-                        <textarea 
-                            id="mensagem" 
-                            name="mensagem" 
-                            value={formData.mensagem} 
-                            onChange={handleChange} 
-                            required 
-                            />
-                    </div>
-                        <button type="submit">Peça seu orçamento</button>
-                </form>
+  return (
+    <div style={{ height: '100%' }}>
+      <Header />
+      <div className={styles.body}>
+        <div className={styles.formContainer}> {/* Aplicando a classe para estilização */}
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="nome">Nome:</label>
+              <input
+                type="text"
+                id="nome"
+                name="nome"
+                placeholder='Digite seu nome'
+                className={styles.input}
+                value={formData.nome}
+                onChange={handleChange}
+                required
+              />
             </div>
+            <div>
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                placeholder='Digite seu email'
+                id="email"
+                className={styles.input}
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="mensagem">Mensagem:</label>
+              <textarea
+                id="mensagem"
+                placeholder='Digite sua mensagem'
+                className={`${styles.messageInput} ${styles.input}`}
+                name="mensagem"
+                value={formData.mensagem}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <button type="submit">Peça seu orçamento</button>
+          </form>
         </div>
-        </>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Contato;
