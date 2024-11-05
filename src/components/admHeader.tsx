c8366ecd6c7a36cf41abc70a54e977d41a7fee98
lorenzo weber <lcweber@inf.ufsm.br>
2024-11-05 10:22:38 -0300

    a

--- src/components/admHeader.tsx
+++ src/components/admHeader.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import s from './admHeader.module.css';
+import s from './admHeader.module.css'; 
 import logo from "../assets/logo.png";
 
 const admHeader = () => {
-    return (
-        <header>
-            <nav>
-                <Link to="/adm/infoManager"><img src={logo} className={s.logo} alt='' /></Link>
-                <Link to="/adm" className={s.button}><p>Log out</p></Link>
-            </nav>
-        </header>
-    );
+  return (
+    <header>
+      <nav>
+        <Link to="/adm/infoManager"><img src={logo} className={s.logo} alt=''/></Link>
+        <ul>
+            <Link to="/adm/addManager" className={s.addMine}><li>Adicionar mina</li></Link>
+            <Link to="/adm"><li>Logout</li></Link>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default admHeader;
