eba8ba4f9f52f56b47adcd623343933f3a7b00f8
Gregori Rubin <gregori.rubin@gmail.com>
2024-11-14 11:26:07 -0300

    feat: adicionar dropdown de usuário com opções de conta e logout nos cabeçalhos do gerente e administrador

--- src/components/ManagerHeader.tsx
+++ src/components/ManagerHeader.tsx
@@ -4,14 +4,27 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './ManagerHeader.module.css';
 import logo from "../assets/logo.png";
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { Dropdown } from 'react-bootstrap';
+import { useAuth } from '../Hooks/AuthContext';
 
 const ManagerHeader = () => {
+  const { signOut } = useAuth();
+
     return (
         <header>
           <nav>
             <Link to="/landing"><img src={logo} className={styles.logo} alt=''/></Link>
             <ul>
-                <Link to="/Manager/Account"> <li> <FontAwesomeIcon icon={faUser} />  </li></Link>  
+                <Dropdown>
+                  <Dropdown.Toggle>
+                    
+                    <FontAwesomeIcon icon={faUser} />
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="/manager/account">Minha Conta</Dropdown.Item>
+                    <Dropdown.Item onClick={signOut}>Sair</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>  
             </ul>
           </nav>
         </header>
--- src/components/admHeader.tsx
+++ src/components/admHeader.tsx
@@ -1,20 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import s from './admHeader.module.css'; 
+import s from './AdmHeader.module.css'; 
 import logo from "../assets/logo.png";
+import { Dropdown } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { useAuth } from '../Hooks/AuthContext';
 
-const admHeader = () => {
+const AdmHeader = () => {
+  const { signOut } = useAuth();
+
   return (
     <header>
       <nav>
-        <Link to="/adm/infoManager"><img src={logo} className={s.logo} alt=''/></Link>
+        <Link to="/admin"><img src={logo} className={s.logo} alt=''/></Link>
         <ul>
-            <Link to="/adm/addManager" className={s.addMine}><li>Adicionar mina</li></Link>
-            <Link to="/adm"><li>Logout</li></Link>
+            <Link to="/admin/addManager" className={s.addMine}><li>Adicionar mina</li></Link>
+            <Dropdown>
+                  <Dropdown.Toggle>
+                    
+                    <FontAwesomeIcon icon={faUser} />
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="/manager/account">Minha Conta</Dropdown.Item>
+                    <Dropdown.Item onClick={signOut}>Sair</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>  
         </ul>
       </nav>
     </header>
   );
 };
 
-export default admHeader;
+export default AdmHeader;
