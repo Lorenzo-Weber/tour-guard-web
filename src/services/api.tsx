e0d10fc278cd9f6fcf5d50b53a8857d2844df4ff
Gregori Rubin <gregori.rubin@gmail.com>
2024-11-14 11:25:37 -0300

    feat: add axios API service with response interceptor for token validation

---
+++ src/services/api.tsx
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+const isProd = process.env.NODE_ENV === 'production'
+
+const apiUrl = isProd ? '' : 'http://localhost:3333'
+
+const api = axios.create({
+  baseURL: apiUrl,
+})
+
+api.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      let is_token_valid = localStorage.getItem('@tourguard:is_token_valid')
+      if (is_token_valid && is_token_valid === 'true') {
+        localStorage.setItem('@tourguard:is_token_valid', 'false')
+      }
+    } else {
+      return Promise.reject(error)
+    }
+  },
+)
+
+export default api
