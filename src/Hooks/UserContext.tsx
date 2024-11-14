239a33106a2256878a7db3009197acae23fe921d
Gregori Rubin <gregori.rubin@gmail.com>
2024-11-14 11:24:47 -0300

    feat: add UserContext for user state management

---
+++ src/Hooks/UserContext.tsx
@@ -0,0 +1,36 @@
+import React, { createContext, useContext, useState } from "react"; 
+
+interface IUser {
+  id: string;
+  full_name: string;
+  email: string;
+}
+
+interface IUserContextData {
+  user: IUser | null;
+  setUser(user: IUser | null): void;
+}
+
+// Tipando o contexto diretamente com IUserContextData
+const UserContext = createContext<IUserContextData>({} as IUserContextData);
+
+const UserProvider: React.FC<any> = ({ children }) => {
+  // Inicializando o estado `user` como null, para sinalizar que não há usuário inicialmente
+  const [user, setUser] = useState<IUser | null>(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+function useUser(): IUserContextData {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error('useUser must be used within a UserProvider');
+  }
+  return context;
+}
+
+export { UserProvider, useUser };
