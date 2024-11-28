41a1a6d8e0efce02d38223845adb71b634e2723c
Gregori Rubin <gregori.rubin@gmail.com>
2024-11-28 10:43:54 -0300

    fix: atualizar URL da API para ambiente de produção e corrigir formatação de código

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,27 +1,29 @@
-import axios from 'axios'
+import axios from "axios";
 
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === "production";
 
-const apiUrl = isProd ? '' : 'http://localhost:3333'
+const apiUrl = isProd
+  ? "https://tour-guard-b28d110234e5.herokuapp.com"
+  : "http://localhost:3000";
 
 const api = axios.create({
   baseURL: apiUrl,
-})
+});
 
 api.interceptors.response.use(
   (response) => {
-    return response
+    return response;
   },
   (error) => {
     if (error.response && error.response.status === 401) {
-      let is_token_valid = localStorage.getItem('@tourguard:is_token_valid')
-      if (is_token_valid && is_token_valid === 'true') {
-        localStorage.setItem('@tourguard:is_token_valid', 'false')
+      let is_token_valid = localStorage.getItem("@tourguard:is_token_valid");
+      if (is_token_valid && is_token_valid === "true") {
+        localStorage.setItem("@tourguard:is_token_valid", "false");
       }
     } else {
-      return Promise.reject(error)
+      return Promise.reject(error);
     }
-  },
-)
+  }
+);
 
-export default api
+export default api;
