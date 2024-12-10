0d302c151171c132e62bfecc87ed09c1dfe6ef9e
Gregori Rubin <gregori.rubin@gmail.com>
2024-12-10 15:18:32 -0300

    feat: ajustar estilos do cabeçalho e melhorar a estrutura do código

--- src/components/ManagerHeader.tsx
+++ src/components/ManagerHeader.tsx
@@ -10,10 +10,19 @@ const ManagerHeader = () => {
   return (
     <header>
       <nav>
-        <Link to="/manager"><img src={logo}  alt='' style={{maxWidth: '150px'}} /></Link>
+        <Link to="/manager">
+          <img src={logo} alt="" style={{ maxWidth: "150px" }} />
+        </Link>
         <ul>
-            <Link to="/manager/account"><li>Minha conta</li></Link>
-            <Link to='' onClick={signOut}><li>Sair</li></Link>
+          <Link to="/manager/mines">
+            <li>Minas</li>
+          </Link>
+          <Link to="/manager/account">
+            <li>Minha conta</li>
+          </Link>
+          <Link to="" onClick={signOut}>
+            <li>Sair</li>
+          </Link>
         </ul>
       </nav>
     </header>
